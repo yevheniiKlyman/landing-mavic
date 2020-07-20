ab1288409d036d819b4cb9d2bc3e240f2f4f65e0
eKliman <kliman.evgeniy@gmail.com>
2020-07-20 15:46:13 +0300

    Made the third and fourth sections and added animation

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,67 @@ import '../../node_modules/normalize.css/normalize.css';
 import '../../node_modules/slick-carousel/slick/slick.css';
 import '../../node_modules/slick-carousel/slick/slick.js';
 import '../scss/style.scss';
-import $ from 'jquery';
+import '../js/slider.js';
 
-$(document).ready(function () {
-  $('.about__slider').slick({
-    prevArrow:
-      '<button class="about__slider-btn about__slider-btn-left"><svg class="about__left-arrow" width="9" height="16"><use xlink:href="images/icons.svg#left-arrow"></use></svg></button>',
-    nextArrow:
-      '<button class="about__slider-btn about__slider-btn-right"><svg class="about__right-arrow" width="9" height="16"><use xlink:href="images/icons.svg#right-arrow"></use></svg></button>',
-    infinite: false,
+const html = document.querySelector('html');
+const top = document.getElementById('top');
+const about = document.getElementById('about');
+const advantages = document.getElementById('advantages');
+const specification = document.getElementById('specification');
+const topItemsForAnimate = top.querySelectorAll('.top-animate');
+const aboutItemsForAnimate = about.querySelectorAll('.about-animate');
+const advantagesItemsForAnimate = advantages.querySelectorAll(
+  '.advantages-animate'
+);
+const specificationItemsForAnimate = specification.querySelectorAll(
+  '.specification-animate'
+);
+
+function checkScrollPosition(section) {
+  if (
+    html.scrollTop >= section.offsetTop - section.offsetHeight / 2 &&
+    html.scrollTop < section.offsetTop + section.offsetHeight / 2
+  ) {
+    return true;
+  }
+}
+
+function addAnimate(items) {
+  items.forEach((item) => {
+    item.classList.add('animate');
   });
-});
+}
+
+function animateTop() {
+  if (checkScrollPosition(top)) {
+    addAnimate(topItemsForAnimate);
+  }
+}
+
+function animateAbout() {
+  if (checkScrollPosition(about)) {
+    addAnimate(aboutItemsForAnimate);
+  }
+}
+
+function animateAdvantages() {
+  if (checkScrollPosition(advantages)) {
+    addAnimate(advantagesItemsForAnimate);
+  }
+}
+
+function animateSpecification() {
+  if (checkScrollPosition(specification)) {
+    addAnimate(specificationItemsForAnimate);
+  }
+}
+
+function scrollHandler() {
+  animateTop();
+  animateAbout();
+  animateAdvantages();
+  animateSpecification();
+}
+
+window.addEventListener('load', scrollHandler);
+document.addEventListener('scroll', scrollHandler);
