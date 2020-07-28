36901d789d4f42fe46999835fcfee16335b4296f
eKliman <kliman.evgeniy@gmail.com>
2020-07-28 15:20:41 +0300

    Made other sections, animation and full screen scrolling for computers and mobile devices

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,19 @@ import '../../node_modules/slick-carousel/slick/slick.css';
 import '../../node_modules/slick-carousel/slick/slick.js';
 import '../scss/style.scss';
 import '../js/slider.js';
+import '../js/accordion.js';
+import '../js/animation.js';
+import { delay } from '../js/animation';
 
 const html = document.querySelector('html');
+const headerInner = document.getElementById('header-inner');
+const scrollArrow = document.querySelectorAll('#scroll-arrow-link');
 const top = document.getElementById('top');
 const about = document.getElementById('about');
 const advantages = document.getElementById('advantages');
 const specification = document.getElementById('specification');
+const questions = document.getElementById('questions');
+const contacts = document.getElementById('contacts');
 const topItemsForAnimate = top.querySelectorAll('.top-animate');
 const aboutItemsForAnimate = about.querySelectorAll('.about-animate');
 const advantagesItemsForAnimate = advantages.querySelectorAll(
@@ -17,15 +24,24 @@ const advantagesItemsForAnimate = advantages.querySelectorAll(
 const specificationItemsForAnimate = specification.querySelectorAll(
   '.specification-animate'
 );
+const questionsItemsForAnimate = questions.querySelectorAll(
+  '.questions-animate'
+);
+const contactsItemsForAnimate = contacts.querySelectorAll('.contacts-animate');
 
-function checkScrollPosition(section) {
-  if (
-    html.scrollTop >= section.offsetTop - section.offsetHeight / 2 &&
-    html.scrollTop < section.offsetTop + section.offsetHeight / 2
-  ) {
-    return true;
-  }
-}
+const dataForAnimate = [
+  [top, topItemsForAnimate],
+  [about, aboutItemsForAnimate],
+  [advantages, advantagesItemsForAnimate],
+  [specification, specificationItemsForAnimate],
+  [questions, questionsItemsForAnimate],
+  [contacts, contactsItemsForAnimate],
+];
+
+let currentSectionIndex, prevScrollTop;
+let startCoords = 0;
+let endCoords = 0;
+let touchDistance = 0;
 
 function addAnimate(items) {
   items.forEach((item) => {
@@ -33,36 +49,103 @@ function addAnimate(items) {
   });
 }
 
-function animateTop() {
-  if (checkScrollPosition(top)) {
-    addAnimate(topItemsForAnimate);
+function defineSection(section) {
+  if (
+    html.scrollTop >= section.offsetTop - section.offsetHeight / 2 &&
+    html.scrollTop < section.offsetTop + section.offsetHeight / 2
+  ) {
+    return section;
   }
 }
 
-function animateAbout() {
-  if (checkScrollPosition(about)) {
-    addAnimate(aboutItemsForAnimate);
+const checkChangeScroll = () => {
+  return new Promise((resolve) => {
+    let scrollChange = setInterval(() => {
+      if (prevScrollTop != html.scrollTop) {
+        prevScrollTop = html.scrollTop;
+      } else {
+        clearInterval(scrollChange);
+        resolve();
+      }
+    }, 500);
+  });
+};
+
+async function checkEndScroll() {
+  await checkChangeScroll();
+  document.removeEventListener('scroll', checkEndScroll);
+  loadHandler();
+}
+
+async function navigationHandler() {
+  if (event.target.closest('a')) {
+    document.addEventListener('scroll', checkEndScroll), { once: true };
   }
 }
 
-function animateAdvantages() {
-  if (checkScrollPosition(advantages)) {
-    addAnimate(advantagesItemsForAnimate);
+function loadHandler() {
+  dataForAnimate.forEach((section) => {
+    if (defineSection(section[0])) {
+      console.log('fire if');
+      currentSectionIndex = dataForAnimate.indexOf(section);
+      html.scrollTop = dataForAnimate[currentSectionIndex][0].offsetTop;
+      addAnimate(section[1]);
+    }
+  });
+}
+
+async function scrollHandler() {
+  document.removeEventListener('wheel', scrollHandler);
+
+  if (event.deltaY > 0 && currentSectionIndex < dataForAnimate.length - 1) {
+    currentSectionIndex += 1;
+    html.scrollTop = dataForAnimate[currentSectionIndex][0].offsetTop;
+    addAnimate(dataForAnimate[currentSectionIndex][1]);
+  } else if (event.deltaY < 0 && currentSectionIndex > 0) {
+    currentSectionIndex -= 1;
+    html.scrollTop = dataForAnimate[currentSectionIndex][0].offsetTop;
+    addAnimate(dataForAnimate[currentSectionIndex][1]);
+  }
+  await delay(1000);
+  document.addEventListener('wheel', scrollHandler);
+}
+
+function scrollArrowHandler() {
+  currentSectionIndex += 1;
+  addAnimate(dataForAnimate[currentSectionIndex][1]);
+}
+
+function touchStartHandler() {
+  startCoords = event.changedTouches[0].pageY;
+}
+
+function touchEndHandler() {
+  endCoords = event.changedTouches[0].pageY;
+  touchDistance = endCoords - startCoords;
+  if (touchDistance < -10 && currentSectionIndex < dataForAnimate.length - 1) {
+    currentSectionIndex += 1;
+    html.scrollTop = dataForAnimate[currentSectionIndex][0].offsetTop;
+    addAnimate(dataForAnimate[currentSectionIndex][1]);
+  } else if (touchDistance > 10 && currentSectionIndex > 0) {
+    currentSectionIndex -= 1;
+    html.scrollTop = dataForAnimate[currentSectionIndex][0].offsetTop;
+    addAnimate(dataForAnimate[currentSectionIndex][1]);
   }
 }
 
-function animateSpecification() {
-  if (checkScrollPosition(specification)) {
-    addAnimate(specificationItemsForAnimate);
-  }
-}
+window.addEventListener('load', loadHandler);
+document.addEventListener('wheel', scrollHandler);
+headerInner.addEventListener('click', navigationHandler);
 
-function scrollHandler() {
-  animateTop();
-  animateAbout();
-  animateAdvantages();
-  animateSpecification();
-}
-
-window.addEventListener('load', scrollHandler);
-document.addEventListener('scroll', scrollHandler);
+const createArrowEventListener = () => {
+  scrollArrow.forEach((arrow) => {
+    arrow.addEventListener('click', scrollArrowHandler);
+  });
+};
+createArrowEventListener();
+document.addEventListener('touchstart', () => {
+  touchStartHandler();
+});
+document.addEventListener('touchend', () => {
+  touchEndHandler();
+});
